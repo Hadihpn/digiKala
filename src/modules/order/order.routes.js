e5ebfbd15bb974ca568781a3c88a32b164facfcb
persnb <persnb@gmail.com>
2025-05-09 11:09:06 +0330

    add set level(packed,intransition,deliverd,canceled) function for orders

--- src/modules/order/order.routes.js
+++ src/modules/order/order.routes.js
@@ -1,7 +1,11 @@
-const { getMyOrderHandler, getOrderByIdHandler } = require("./order.service")
+const { getMyOrderHandler, getOrderByIdHandler, setPackedStatusToOrder, setInTransitStatusToOrder, setDeliveredStatusToOrder, setCanceledStatusToOrder } = require("./order.service")
 const Authorization = require("../../guard/auhtorization.guard");
 
 const router = require("express").Router()
+router.patch("/set-packed/:id",Authorization,setPackedStatusToOrder)
+router.patch("/set-in-transition/:id",Authorization,setInTransitStatusToOrder)
+router.patch("/set-deliverd/:id",Authorization,setDeliveredStatusToOrder)
+router.patch("/set-canceled/:id",Authorization,setCanceledStatusToOrder)
 router.get("/",Authorization,getMyOrderHandler)
 router.get("/:id",Authorization,getOrderByIdHandler)
 module.exports  ={
--- src/modules/order/order.service.js
+++ src/modules/order/order.service.js
@@ -1,7 +1,11 @@
 const { OrderStatus } = require("../../common/order.const");
 const createHttpError = require("http-error");
 const { Order, OrderItems } = require("./order.model");
-const { Product, ProductColor, ProductSize } = require("../product/product.model");
+const {
+  Product,
+  ProductColor,
+  ProductSize,
+} = require("../product/product.model");
 
 async function getMyOrderHandler(req, res, next) {
   try {
@@ -20,27 +24,99 @@ async function getMyOrderHandler(req, res, next) {
   }
 }
 async function getOrderByIdHandler(req, res, next) {
+  try {
+    const { id: userId } = req.user;
+    const { id } = req.params;
+    if (!id) throw createHttpError(400, "no order founded");
+    const order = await Order.findOne({
+      where: { id, userId },
+      include: {
+        model: OrderItems,
+        as: "items",
+        include: [
+          {
+            model: Product,
+            as: "product",
+          },
+          {
+            model: ProductColor,
+            as: "color",
+          },
+          {
+            model: ProductSize,
+            as: "size",
+          },
+        ],
+      },
+    });
+    return res.json({
+      orders,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+async function setPackedStatusToOrder(req, res, next) {
+  try {
+    const { id } = req.params;
+    const order = await Order.findByPk(id);
+    if (!order) throw createHttpError(400, "no order was found");
+    if (order.status !== OrderStatus.InProcess)
+      throw createHttpError(400, "at first make a order plz");
+    order.status = OrderStatus.Packed;
+    await order.save();
+    return res.json({
+      message: "order packed successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+async function setInTransitStatusToOrder(req, res, next) {
     try {
-      const { id: userId } = req.user;
       const { id } = req.params;
-      if (!id)
-        throw createHttpError(400, "no order founded");
-      const order = await Order.findOne({
-        where: {  id,userId },
-        include:{model:OrderItems,as:"items",include:[
-            {
-                model:Product,as:"product",
-            },
-            {
-                model:ProductColor,as:"color",
-            },
-            {
-                model:ProductSize,as:"size",
-            }
-        ]}
-      });
+      const order = await Order.findByPk(id);
+      if (!order) throw createHttpError(400, "no order was found");
+      if (order.status !== OrderStatus.Packed)
+        throw createHttpError(400, "at first packed the order plz");
+      order.status = OrderStatus.InTransit;
+      await order.save();
       return res.json({
-        orders,
+        message: "order went for transition successfully",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async function setDeliveredStatusToOrder(req, res, next) {
+    try {
+      const { id } = req.params;
+      const order = await Order.findByPk(id);
+      if (!order) throw createHttpError(400, "no order was found");
+      if (order.status !== OrderStatus.InTransit)
+        throw createHttpError(400, "at first pass transition level of order plz");
+      order.status = OrderStatus.Delivered;
+      await order.save();
+      return res.json({
+        message: "order delivered successfully",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async function setCanceledStatusToOrder(req, res, next) {
+    try {
+      const { id } = req.params;
+      const {reason} = req.body;
+      const order = await Order.findByPk(id);
+      if (!order) throw createHttpError(400, "no order was found");
+      if (order.status !== OrderStatus.Delivered)
+        throw createHttpError(400, "cannot canceled delivered order ");
+      order.status = OrderStatus.Canceled;
+      order.reason = reason;
+      await order.save();
+      return res.json({
+        message: "order delivered successfully",
       });
     } catch (error) {
       next(error);
@@ -48,5 +124,9 @@ async function getOrderByIdHandler(req, res, next) {
   }
 module.exports = {
   getMyOrderHandler,
-  getOrderByIdHandler
+  getOrderByIdHandler,
+  setPackedStatusToOrder,
+  setInTransitStatusToOrder,
+  setDeliveredStatusToOrder,
+  setCanceledStatusToOrder
 };
