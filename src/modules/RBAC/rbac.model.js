65cea8208ab8f93e2f16ca2c4fff3ea3c4ab9e5b
persnb <persnb@gmail.com>
2025-05-09 13:31:49 +0330

    addPermissionToRole

--- src/modules/RBAC/rbac.model.js
+++ src/modules/RBAC/rbac.model.js
@@ -11,9 +11,14 @@ const Permission = sequelize.define("permission", {
     title: {type: DataTypes.STRING, allowNull: false},
     description: {type: DataTypes.STRING, allowNull: true},
 }, {timestamps: false, modelName: "permission"});
-
+const RolePermission = sequelize.define("role_permission", {
+    id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true},
+    roleId: {type: DataTypes.INTEGER, allowNull: false},
+    permissionId: {type: DataTypes.INTEGER, allowNull: false},
+}, {timestamps: false, modelName: "role_permission"});
 
 module.exports = {
     Role,
     Permission,
+    RolePermission
 };
